47d1505d294502a0de5a2605625a414f3bd7cd62
KimKyeongJe <kk3466@columbia.edu>
2020-11-16 03:38:13 -0500

    Add test function

--- tests/test_cipher_kk3466.py
+++ tests/test_cipher_kk3466.py
@@ -3,3 +3,11 @@ from cipher_kk3466 import cipher_kk3466
 
 def test_version():
     assert __version__ == '0.1.0'
+    
+    
+def test_cipher():
+    example = 'cats'
+    shift = 1
+    return cipher(example, shift)
+    
+test_cipher()
